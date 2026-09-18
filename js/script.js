14e416e232ee4db604ac5efd958862db53180a8e
nahlawat008-blip <nahlawat008@gmail.com>
2026-09-18 21:06:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1383,6 +1383,48 @@ function loadPets(data) {
 
         `;
 
+        return `
+
+    <div class="pet-card">
+
+        <div class="pet-avatar">
+            ${icon}
+        </div>
+
+        <div style="flex:1;">
+
+            <h4>
+                ${pet.name}
+            </h4>
+
+            <p>
+                ${pet.type} • ${pet.breed}
+            </p>
+
+            <p>
+                ${pet.age} years •
+                ${pet.gender}
+            </p>
+
+            <button
+                onclick="deletePet(${pet.id})"
+                style="
+                    margin-top:10px;
+                    padding:7px 12px;
+                    border:none;
+                    border-radius:6px;
+                    cursor:pointer;
+                "
+            >
+                🗑️ Delete Pet
+            </button>
+
+        </div>
+
+    </div>
+
+`;
+        
         return;
 
     }
