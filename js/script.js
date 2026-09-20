c3d5a10d8456f21a63f2b665920b9005f242d41d
nahlawat008-blip <nahlawat008@gmail.com>
2026-09-20 08:42:26 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ cards.forEach(card => {
 const whatsapp = document.createElement("a");
 whatsapp.href = "https://wa.me/919772333320";
 whatsapp.target = "_blank";
-WhatsApp.innerHTML = '<i class="fa-brands fa-whatsapp"></i>';
+whatsapp.innerHTML = "💬";
 whatsapp.className = "floating-whatsapp";
 
 document.body.appendChild(whatsapp);
