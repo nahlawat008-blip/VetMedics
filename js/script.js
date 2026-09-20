ac5c115b405afe973636e60691c7fd7119eb4220
nahlawat008-blip <nahlawat008@gmail.com>
2026-09-20 20:44:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const GOOGLE_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbxFUsh7KSHub44VS0U6SsVszpp90_Ns2eK-cyZ9gxEdgnOVHm8nuX87yDxB6JchkNoUTQ/exec"
+    "https://script.google.com/macros/s/AKfycbwqckWul5n5XN9cse1GKvZfymCJ70GttLYaxD6LStlfRrsJoLVN198kp_hPU9vZzv4ctg/exec"
 
 // =========================
 // Vet Medics Pet Clinic
